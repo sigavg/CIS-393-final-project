9c09bbdf262c645d757c0ac84cb4d3831fbdc8f0
sigavg <lesimplu@gmail.com>
2023-05-03 00:40:26 -0500

    5/2

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ var mysql = require('mysql');
 var host = 'localhost';
 var user = 'root';
 var database = 'final393';
+var port = 3000;
 
 var con = mysql.createConnection({host, user, database});
 
 con.connect(function(err){
     if (err) throw err;
-    console.log("connected");
+    console.log("connected to "+database);
 });
 
 //body parser
@@ -23,40 +24,89 @@ var bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({extended:true}));
 
 //urls
+// app.get("/update", function(req, res){
+//     res.render("update");
+//     //TODO this page should have a name field, radio button for name/connection, image
+// })
+
+// app.get("/delete", function(req, res){
+//     res.render("delete");
+//     //TODO only needs a person field
+// })
+
+//TODO Redo the home page so: it downloads all the names and then does buttons
 
 
-app.get("/insert", function(req, res){
+//Insert function
+app.get("/insert", function(req, res) {
     res.render("insert");
-    //TODO this page has to have a name field, file field (jpg only), and connection field
-})
+});
+app.post("/insert", function(req, res){
+    var newname = req.body.new_name;
+    var extname = req.body.existing_name;
 
-app.get("/update", function(req, res){
-    res.render("update");
-    //TODO this page should have a name field, radio button for name/connection, image
-})
+    var newid =''; var extid=''; var filename;
 
-app.get("/delete", function(req, res){
-    res.render("delete");
-    //TODO only needs a person field
-})
+    //insert new name into people table
+    var q = "INSERT INTO people (name) VALUES (\'"+newname+"\');";
+    console.log(q);
+    con.query(q, function (error, results) {
+        if (error) throw error;
+    });
 
-//TODO Redo the home page so: it  
+    //get existing person ID
+    q = "SELECT id FROM people WHERE name=\'"+extname+"\';";
+    console.log(q);
+    con.query(q, function(error, results) {
+        if (error) throw error;
+        extid = '' + results[0].id;
+        console.log(extid);
+        console.log(results[0].id)
+    });
+
+    //get new person ID
+    q = "SELECT id FROM people WHERE name=\'"+newname+"\';";
+    console.log(q);
+    con.query(q, function(error, results) {
+        if (error) throw error;
+        newid = '' + results[0].id;
+        console.log(newid);
+        console.log(results[0].id)
+    });
+    
+    //create the photo file name
+    if (newid < extid)
+    {   filename = ''+newid+'-'+extid+'.jpg';}
+    else
+    {   filename = ''+extid+'-'+newid+'.jpg';}
 
 
+    //insert connections
+    q = "INSERT INTO connections(p1id, p2id, filename) VALUES("+extid+", "+newid+",\'"+filename+"\'),("+newid+", "+extid+",\'"+filename+"\');";
+    console.log(q);
+    con.query(q, function (error, results) {
+        if (error) throw error;
+        
+    })
 
-//Search function get
+    //rename file
+    q = "SELECT filename FROM connections WHERE p1id ="+extid+" AND p2id ="+newid+";";
+    console.log(q);
+    con.query(q, function(error, results) {
+        if (error) throw error;
+        res.render("insert2",{data:results});
+    });
+});
+
+//Search function
 app.get("/search", function(req, res) {
     res.render("search");
 });
-
-
-//Search function post
 app.post("/search", function(req, res){
-    console.log(req.body.search_term);
     var searcht = req.body.search_term;
-    console.log("Searched: "+searcht)
+
     var q = "SELECT name FROM people WHERE name LIKE  '%"+searcht+"%'  ORDER BY name ASC ;";
-    
+    console.log(q);
     con.query(q, function (error, results) {
         if (error) throw error;
         console.log({data: results});
@@ -65,17 +115,9 @@ app.post("/search", function(req, res){
         else
         {   
             res.render("search_result", {data: results});
-
-            // var msg = 'people: \n';
-            // for(x of results)
-            // {
-            //     msg += x.name + '\n';
-            // }
-            // res.send(msg);
         }
     });
 });
-
 
 //display all
 app.get("/display", function(req, res){
@@ -87,7 +129,7 @@ app.get("/display", function(req, res){
     });
 });
 
-
-app.listen(3000, function(){
-    console.log('listening');
+//listen
+app.listen(port, function(){
+    console.log('listening at '+port);
 })
